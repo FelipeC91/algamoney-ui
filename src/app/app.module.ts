a8b9cb95e35a865b7067e8cd4bedad9c920296df
FelipeC91 <feelipecampos91@gmail.com>
2023-03-12 18:58:46 -0300

    progresso aula 11.8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import {LOCALE_ID} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +18,12 @@ import { faPencil, faTrashCan, faBars, faPlus } from '@fortawesome/free-solid-sv
 import { LaunchSearchComponent } from './launch-search/launch-search.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PersonSearchComponent } from './person-search/person-search.component';
+
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(ptBr);
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,9 +40,12 @@ import { PersonSearchComponent } from './person-search/person-search.component';
     TooltipModule,
     SidebarModule,
     BrowserAnimationsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
